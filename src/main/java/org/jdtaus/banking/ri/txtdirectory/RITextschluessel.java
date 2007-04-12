b9297cdce47ac4323fe769dccb3554bdb8fe0ae7
Christian Schulte <cs@schulte.it>
2007-04-12 12:35:49 +0000

    - updated parent
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/jdtaus-banking-ri-textschluesselverzeichnis/trunk@2611 d3f52c91-230f-0410-802c-e3ec080d4bae

--- src/main/java/org/jdtaus/banking/ri/txtdirectory/RITextschluessel.java
+++ src/main/java/org/jdtaus/banking/ri/txtdirectory/RITextschluessel.java
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-package org.jdtaus.banking.spi.runtime;
+package org.jdtaus.banking.ri.txtdirectory;
 
 import java.text.MessageFormat;
 import java.util.HashMap;
