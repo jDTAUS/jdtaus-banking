a83ea1c2597089b360b2a6332c6cc3cf4e05e990
Christian Schulte <cs@schulte.it>
2007-04-12 12:29:35 +0000

    - updated parent
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/jdtaus-banking-ri-bankleitzahlenverzeichnis/trunk@2607 d3f52c91-230f-0410-802c-e3ec080d4bae

--- src/main/java/org/jdtaus/banking/ri/blzdirectory/BundesbankBankleitzahlenVerzeichnis.java
+++ src/main/java/org/jdtaus/banking/ri/blzdirectory/BundesbankBankleitzahlenVerzeichnis.java
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-package org.jdtaus.banking.spi.runtime;
+package org.jdtaus.banking.ri.blzdirectory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
