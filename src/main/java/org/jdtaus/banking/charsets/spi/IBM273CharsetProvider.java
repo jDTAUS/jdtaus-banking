1aa1723ea6dad3893ac8ff535d4201fc446ffe42
Christian Schulte <cs@schulte.it>
2007-04-22 21:48:48 +0000

    - added charset provider implementations
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/jdtaus-banking-charsets/trunk@2946 d3f52c91-230f-0410-802c-e3ec080d4bae

--- src/main/java/org/jdtaus/banking/charsets/spi/IBM273CharsetProvider.java
+++ src/main/java/org/jdtaus/banking/charsets/spi/IBM273CharsetProvider.java
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-package org.jdtaus.core.nio.util.spi;
+package org.jdtaus.banking.charsets.spi;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
