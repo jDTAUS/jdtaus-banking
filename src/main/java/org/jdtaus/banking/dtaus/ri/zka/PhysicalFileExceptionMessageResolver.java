3ec556c111824562e0d0075a3c3ab1591fb30fdc
Christian Schulte <cs@schulte.it>
2007-12-26 21:46:43 +0000

    Merged changes 4493:4544 from trunk.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/jdtaus-banking-ri-dtaus/branches/jdtaus-banking-ri-dtaus-1.x@4546 d3f52c91-230f-0410-802c-e3ec080d4bae

---
+++ src/main/java/org/jdtaus/banking/dtaus/ri/zka/PhysicalFileExceptionMessageResolver.java
@@ -0,0 +1,157 @@
+/*
+ *  jDTAUS - DTAUS fileformat.
+ *  Copyright (c) 2005 Christian Schulte <cs@schulte.it>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdtaus.banking.dtaus.ri.zka;
+
+import org.jdtaus.banking.dtaus.PhysicalFileException;
+import org.jdtaus.core.container.Dependency;
+import org.jdtaus.core.container.Implementation;
+import org.jdtaus.core.container.ModelFactory;
+import org.jdtaus.core.container.Properties;
+import org.jdtaus.core.container.Property;
+import org.jdtaus.core.lang.util.ExceptionMessageResolver;
+import org.jdtaus.core.text.Message;
+import org.jdtaus.core.text.Messages;
+
+/**
+ * {@code ExceptionMessageResolver} producing {@code Message}s for
+ * {@code PhysicalFileException} instances.
+ *
+ * @author <a href="mailto:cs@schulte.it">Christian Schulte</a>
+ * @version $Id$
+ *
+ * @see ExceptionMessageResolver
+ */
+public class PhysicalFileExceptionMessageResolver
+    implements ExceptionMessageResolver
+{
+    //--Implementation----------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausImplementation
+    // This section is managed by jdtaus-container-mojo.
+
+    /** Meta-data describing the implementation. */
+    private static final Implementation META =
+        ModelFactory.getModel().getModules().
+        getImplementation(PhysicalFileExceptionMessageResolver.class.getName());
+// </editor-fold>//GEN-END:jdtausImplementation
+
+    //----------------------------------------------------------Implementation--
+    //--Constructors------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausConstructors
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * <code>PhysicalFileExceptionMessageResolver</code> implementation constructor.
+     *
+     * @param meta Implementation meta-data.
+     *
+     * @throws NullPointerException if <code>meta</code> is <code>null</code>.
+     */
+    private PhysicalFileExceptionMessageResolver(final Implementation meta)
+    {
+        super();
+        if(meta == null)
+        {
+            throw new NullPointerException("meta");
+        }
+        this.initializeProperties(meta.getProperties());
+    }
+    /**
+     * <code>PhysicalFileExceptionMessageResolver</code> dependency constructor.
+     *
+     * @param meta dependency meta-data.
+     *
+     * @throws NullPointerException if <code>meta</code> is <code>null</code>.
+     */
+    private PhysicalFileExceptionMessageResolver(final Dependency meta)
+    {
+        super();
+        if(meta == null)
+        {
+            throw new NullPointerException("meta");
+        }
+        this.initializeProperties(meta.getProperties());
+    }
+
+    /**
+     * Initializes the properties of the instance.
+     *
+     * @param meta the property values to initialize the instance with.
+     *
+     * @throws NullPointerException if {@code meta} is {@code null}.
+     */
+    private void initializeProperties(final Properties meta)
+    {
+        Property p;
+
+        if(meta == null)
+        {
+            throw new NullPointerException("meta");
+        }
+
+    }
+// </editor-fold>//GEN-END:jdtausConstructors
+
+    //------------------------------------------------------------Constructors--
+    //--Dependencies------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausDependencies
+    // This section is managed by jdtaus-container-mojo.
+
+// </editor-fold>//GEN-END:jdtausDependencies
+
+    //------------------------------------------------------------Dependencies--
+    //--Properties--------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausProperties
+    // This section is managed by jdtaus-container-mojo.
+
+// </editor-fold>//GEN-END:jdtausProperties
+
+    //--------------------------------------------------------------Properties--
+    //--ExceptionMessageResolver------------------------------------------------
+
+    public Message[] resolve(final Exception exception)
+    {
+        Message[] resolved = null;
+
+        if(exception != null && exception instanceof PhysicalFileException)
+        {
+            final PhysicalFileException e = (PhysicalFileException) exception;
+            final Messages msgs = new Messages();
+            msgs.addMessages(e.getMessages());
+            resolved = msgs.getMessages();
+        }
+
+        return resolved;
+    }
+
+    //------------------------------------------------ExceptionMessageResolver--
+    //--PhysicalFileExceptionMessageResolver------------------------------------
+
+    /** Creates a new {@code PhysicalFileExceptionMessageResolver} instance. */
+    public PhysicalFileExceptionMessageResolver()
+    {
+        this(META);
+    }
+
+    //------------------------------------PhysicalFileExceptionMessageResolver--
+}
