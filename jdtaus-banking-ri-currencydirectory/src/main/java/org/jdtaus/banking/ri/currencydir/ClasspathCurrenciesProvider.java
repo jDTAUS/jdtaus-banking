769abf3dc372e219e7fa581cc4c50033fdf42a8a
Christian Schulte <cs@schulte.it>
2008-12-29 22:26:59 +0000

    o Moved to branch directory.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/branches/jdtaus-banking-1.x@5871 d3f52c91-230f-0410-802c-e3ec080d4bae

---
+++ jdtaus-banking-ri-currencydirectory/src/main/java/org/jdtaus/banking/ri/currencydir/ClasspathCurrenciesProvider.java
@@ -0,0 +1,202 @@
+/*
+ *  jDTAUS Banking RI CurrencyDirectory
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  Christian Schulte, Haldener Strasse 72, 58095 Hagen, Germany
+ *  <cs@jdtaus.org> (+49 2331 3543887)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdtaus.banking.ri.currencydir;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.container.PropertyException;
+
+/**
+ * Classpath {@code CurrenciesProvider} implementation.
+ * <p>This implementation provides resources by searching the classpath.
+ * Property {@code resourceName} holds the name of the resources to search and
+ * defaults to {@code META-INF/jdtaus/currencies.xml}.</p>
+ *
+ * @author <a href="mailto:cs@schulte.it">Christian Schulte</a>
+ * @version $Id$
+ *
+ * @see XMLCurrencyDirectory
+ */
+public final class ClasspathCurrenciesProvider implements CurrenciesProvider
+{
+    //--Constructors------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausConstructors
+    // This section is managed by jdtaus-container-mojo.
+
+    /** Standard implementation constructor <code>org.jdtaus.banking.ri.currencydir.ClasspathCurrenciesProvider</code>. */
+    public ClasspathCurrenciesProvider()
+    {
+        super();
+    }
+
+// </editor-fold>//GEN-END:jdtausConstructors
+
+    //------------------------------------------------------------Constructors--
+    //--Properties--------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausProperties
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the value of property <code>defaultResourceName</code>.
+     *
+     * @return Default name of the resources to provide.
+     */
+    private java.lang.String getDefaultResourceName()
+    {
+        return (java.lang.String) ContainerFactory.getContainer().
+            getProperty( this, "defaultResourceName" );
+
+    }
+
+// </editor-fold>//GEN-END:jdtausProperties
+
+    //--------------------------------------------------------------Properties--
+    //--CurrenciesProvider------------------------------------------------------
+
+    public URL[] getResources() throws IOException
+    {
+        this.assertValidProperties();
+        final Collection col = new LinkedList();
+        final Enumeration en =
+            this.getClassLoader().getResources( this.getResourceName() );
+
+        while ( en.hasMoreElements() )
+        {
+            col.add( en.nextElement() );
+        }
+
+        return (URL[]) col.toArray( new URL[ col.size() ] );
+    }
+
+    //------------------------------------------------------CurrenciesProvider--
+    //--ClasspathCurrenciesProvider---------------------------------------------
+
+    /** Classloader searched for resources. */
+    private ClassLoader classLoader;
+
+    /** Name of the classpath resource to search. */
+    private String resourceName;
+
+    /**
+     * Creates a new {@code ClasspathCurrenciesProvider} instance taking
+     * the name of the classpath resources to search.
+     *
+     * @param resourceName name of the classpath resources to search.
+     */
+    public ClasspathCurrenciesProvider( final String resourceName )
+    {
+        this( resourceName, null );
+    }
+
+    /**
+     * Creates a new {@code ClasspathCurrenciesProvider} instance taking
+     * the classloader to search for resources.
+     *
+     * @param classLoader the classloader to search for resources.
+     */
+    public ClasspathCurrenciesProvider( final ClassLoader classLoader )
+    {
+        this( null, classLoader );
+    }
+
+    /**
+     * Creates a new {@code ClasspathCurrenciesProvider} instance taking the
+     * name of the classpath resources to search and the classloader to search
+     * for resources.
+     *
+     * @param resourceName name of the classpath resources to search.
+     * @param classLoader the classloader to search for resources.
+     */
+    public ClasspathCurrenciesProvider( final String resourceName,
+        final ClassLoader classLoader )
+    {
+        this.resourceName = resourceName;
+        this.classLoader = classLoader;
+    }
+
+    /**
+     * Gets the name of the classpath resource to search.
+     *
+     * @return the name of the classpath resource to search.
+     */
+    public String getResourceName()
+    {
+        if ( this.resourceName == null )
+        {
+            this.resourceName = this.getDefaultResourceName();
+        }
+
+        return this.resourceName;
+    }
+
+    /**
+     * Gets the classloader searched for resources.
+     * <p>This method returns either the current thread's context classloader or
+     * this classes classloader, if the current thread has no context
+     * classloader set. A custom classloader can be specified by using one of
+     * the constructors taking a classloader.</p>
+     *
+     * @return the classloader to search for resources.
+     */
+    public ClassLoader getClassLoader()
+    {
+        if ( this.classLoader == null )
+        {
+            this.classLoader =
+                Thread.currentThread().getContextClassLoader();
+
+        }
+
+        if ( this.classLoader == null )
+        {
+            this.classLoader = this.getClass().getClassLoader();
+        }
+
+        return this.classLoader;
+    }
+
+    /**
+     * Checks configured properties.
+     *
+     * @throws PropertyException if properties hold invalid values.
+     */
+    private void assertValidProperties()
+    {
+        if ( this.getResourceName() == null ||
+            this.getResourceName().length() <= 0 )
+        {
+
+            throw new PropertyException( "resourceName",
+                this.getResourceName() );
+
+        }
+    }
+
+    //---------------------------------------------ClasspathCurrenciesProvider--
+}
---
+++ jdtaus-banking-ri-currencydirectory/src/main/java/org/jdtaus/banking/ri/currencydir/XMLCurrency.java
@@ -0,0 +1,306 @@
+/*
+ *  jDTAUS Banking RI CurrencyDirectory
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  Christian Schulte, Haldener Strasse 72, 58095 Hagen, Germany
+ *  <cs@jdtaus.org> (+49 2331 3543887)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdtaus.banking.ri.currencydir;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Currency.
+ *
+ * @author <a href="mailto:cs@schulte.it">Christian Schulte</a>
+ * @version $Id$
+ */
+public class XMLCurrency implements Serializable, Cloneable
+{
+    //--Constants---------------------------------------------------------------
+
+    /** Serial version UID for backwards compatibility with 1.0.x classes. */
+    private static final long serialVersionUID = 3499875740280116856L;
+
+    //---------------------------------------------------------------Constants--
+    //--XMLCurrency-------------------------------------------------------------
+
+    /**
+     * ISO currency code.
+     * @serial
+     */
+    private String isoCode;
+
+    /**
+     * DTAUS currency code.
+     * @serial
+     */
+    private Character dtausCode;
+
+    /**
+     * Start date.
+     * @serial
+     */
+    private Date startDate;
+
+    /**
+     * End date.
+     * @serial
+     */
+    private Date endDate;
+
+    /** Cached hash-code. */
+    private transient int hashCode = NO_HASHCODE;
+
+    /** Constant for field {@code hashCode} forcing hash code computation. */
+    private static final int NO_HASHCODE = Integer.MIN_VALUE;
+
+    /**
+     * Gets the ISO currency code.
+     *
+     * @return the ISO currency code.
+     */
+    public String getIsoCode()
+    {
+        return this.isoCode;
+    }
+
+    /**
+     * Sets the ISO currency code.
+     *
+     * @param value the ISO currency code.
+     */
+    public void setIsoCode( final String value )
+    {
+        this.isoCode = value;
+        this.hashCode = NO_HASHCODE;
+    }
+
+    /**
+     * Gets the DTAUS currency code.
+     *
+     * @return the DTAUS currency code or {@code null}.
+     */
+    public Character getDtausCode()
+    {
+        return this.dtausCode;
+    }
+
+    /**
+     * Sets the DTAUS currency code.
+     *
+     * @param value the DTAUS currency code or {@code null}.
+     */
+    public void setDtausCode( final Character value )
+    {
+        this.dtausCode = value;
+        this.hashCode = NO_HASHCODE;
+    }
+
+    /**
+     * Gets the start date.
+     *
+     * @return the start date..
+     */
+    public Date getStartDate()
+    {
+        return this.startDate;
+    }
+
+    /**
+     * Sets the start date.
+     *
+     * @param value the start date.
+     */
+    public void setStartDate( final Date value )
+    {
+        this.startDate = value;
+        this.hashCode = NO_HASHCODE;
+    }
+
+    /**
+     * Gets the end date.
+     *
+     * @return the end date..
+     */
+    public Date getEndDate()
+    {
+        return this.endDate;
+    }
+
+    /**
+     * Sets the end date.
+     *
+     * @param value the end date.
+     */
+    public void setEndDate( final Date value )
+    {
+        this.endDate = value;
+        this.hashCode = NO_HASHCODE;
+    }
+
+    /**
+     * Checks that the currency is valid at a given date.
+     *
+     * @param date the date with which to check.
+     *
+     * @return {@code true}, if the currency is valid at {@code date};
+     * {@code false} if not.
+     *
+     * @throws NullPointerException if {@code date} is {@code null}.
+     */
+    public boolean isValidAt( final Date date )
+    {
+        if ( date == null )
+        {
+            throw new NullPointerException( "date" );
+        }
+
+        return ( date.equals( this.getStartDate() ) ||
+            date.after( this.getStartDate() ) ) &&
+            ( this.getEndDate() == null ||
+            date.equals( this.getEndDate() ) ||
+            date.before( this.getEndDate() ) );
+
+    }
+
+    /**
+     * Creates a string representing the properties of the instance.
+     *
+     * @return a string representing the properties of the instance.
+     */
+    private String internalString()
+    {
+        return new StringBuffer( 200 ).append( '{' ).
+            append( "isoCode=" ).append( this.isoCode ).
+            append( ", dtausCode=" ).append( this.dtausCode ).
+            append( ", startDate=" ).append( this.startDate ).
+            append( ", endDate=" ).append( this.endDate ).
+            append( '}' ).toString();
+
+    }
+
+    //-------------------------------------------------------------XMLCurrency--
+    //--Object------------------------------------------------------------------
+
+    /**
+     * Creates and returns a copy of this object.
+     *
+     * @return a clone of this instance.
+     */
+    public Object clone()
+    {
+        try
+        {
+            final XMLCurrency ret = (XMLCurrency) super.clone();
+            if ( this.startDate != null )
+            {
+                ret.startDate = (Date) this.startDate.clone();
+            }
+            if ( this.endDate != null )
+            {
+                ret.endDate = (Date) this.endDate.clone();
+            }
+
+            return ret;
+        }
+        catch ( CloneNotSupportedException e )
+        {
+            throw new AssertionError( e );
+        }
+    }
+
+    /**
+     * Indicates whether some other object is equal to this one by comparing
+     * the values of all properties.
+     *
+     * @param o the reference object with which to compare.
+     *
+     * @return {@code true} if this object is the same as {@code o};
+     * {@code false} otherwise.
+     */
+    public boolean equals( final Object o )
+    {
+        boolean ret = o == this;
+
+        if ( !ret && o instanceof XMLCurrency )
+        {
+            final XMLCurrency that = (XMLCurrency) o;
+            ret =
+                ( this.getIsoCode() == null
+                ? that.getIsoCode() == null
+                : this.getIsoCode().equals( that.getIsoCode() ) ) &&
+                ( this.getDtausCode() == that.getDtausCode() ) &&
+                ( this.getStartDate() == null
+                ? that.getStartDate() == null
+                : this.getStartDate().equals( that.getStartDate() ) ) &&
+                ( this.getEndDate() == null
+                ? that.getEndDate() == null
+                : this.getEndDate().equals( that.getEndDate() ) );
+
+        }
+
+        return ret;
+    }
+
+    /**
+     * Returns a hash code value for this object.
+     *
+     * @return a hash code value for this object.
+     */
+    public int hashCode()
+    {
+        if ( this.hashCode == NO_HASHCODE )
+        {
+            int hc = 23;
+
+            hc = 37 * hc + ( this.dtausCode == null
+                ? 0
+                : (int) this.dtausCode.charValue() );
+
+            hc = 37 * hc + ( this.isoCode == null
+                ? 0
+                : this.isoCode.hashCode() );
+
+            hc = 37 * hc + ( this.startDate == null
+                ? 0
+                : this.startDate.hashCode() );
+
+            hc = 37 * hc + ( this.endDate == null
+                ? 0
+                : this.endDate.hashCode() );
+
+            this.hashCode = hc;
+        }
+
+        return this.hashCode;
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return a string representation of the object.
+     */
+    public String toString()
+    {
+        return super.toString() + this.internalString();
+    }
+
+    //------------------------------------------------------------------Object--
+}
