3508c665dbc39e070e292e12ab48781aea524e4d
Christian Schulte <cs@schulte.it>
2008-12-29 22:23:40 +0000

    o Moved to branch directory.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/branches/jdtaus-banking-1.x@5867 d3f52c91-230f-0410-802c-e3ec080d4bae

---
+++ jdtaus-banking-charsets/src/test/java/org/jdtaus/banking/charsets/spi/test/CharsetsTest.java
@@ -0,0 +1,57 @@
+/*
+ *  jDTAUS Banking Charset Providers
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  Christian Schulte, Haldener Strasse 72, 58095 Hagen, Germany
+ *  <cs@jdtaus.org> (+49 2331 3543887)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdtaus.banking.charsets.spi.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.jdtaus.core.nio.util.Charsets;
+
+/**
+ * Tests the {@code Charsets} implementation.
+ *
+ * @author <a href="mailto:cs@schulte.it">Christian Schulte</a>
+ * @version $Id$
+ */
+public class CharsetsTest extends TestCase
+{
+    //--Tests-------------------------------------------------------------------
+
+    private static final String TEST =
+        "ABCDEFGHIJKLMNOPQRSTUVWXY0123456789äöüß";
+
+    public void testISO646DE() throws Exception
+    {
+        final byte[] encoded = Charsets.encode( TEST, "ISO646-DE" );
+        final String decoded = Charsets.decode( encoded, "ISO646-DE" );
+        Assert.assertEquals( TEST, decoded );
+    }
+
+    public void testIBM273() throws Exception
+    {
+        final byte[] encoded = Charsets.encode( TEST, "IBM273" );
+        final String decoded = Charsets.decode( encoded, "IBM273" );
+        Assert.assertEquals( TEST, decoded );
+    }
+
+    //-------------------------------------------------------------------Tests--
+}
