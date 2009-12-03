7b28ade025129845667a1cc7f338a0ee2eafcb2c
Christian Schulte <cs@schulte.it>
2009-12-03 23:09:21 +0000

    o Added missing classes.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-banking/branches/jdtaus-banking-1.x@8325 d3f52c91-230f-0410-802c-e3ec080d4bae

---
+++ jdtaus-banking-messages/src/main/java/org/jdtaus/banking/messages/ReadsCurrenciesMessage.java
@@ -0,0 +1,96 @@
+/*
+ *  jDTAUS Banking Messages
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.jdtaus.banking.messages;
+
+import java.util.Locale;
+import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.text.Message;
+
+/**
+ * Message stating that currencies are being read.
+ * @author Christian Schulte
+ * @version $Id$
+ */
+public final class ReadsCurrenciesMessage extends Message
+{
+
+    /** Serial version UID for backwards compatibility with 1.11.x classes. */
+    private static final long serialVersionUID = -7704672851654850009L;
+
+    /** Empty array. */
+    private static final Object[] ARGUMENTS =
+    {
+    };
+
+    /** Creates a new {@code ReadsCurrenciesMessage} instance. */
+    public ReadsCurrenciesMessage()
+    {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return An empty array, since the message has no arguments.
+     */
+    public Object[] getFormatArguments( final Locale locale )
+    {
+        return ARGUMENTS;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return The corresponding text from the message's {@code ResourceBundle}
+     * <blockquote><pre>
+     * Reads currencies.
+     * </pre></blockquote>
+     */
+    public String getText( final Locale locale )
+    {
+        return this.getReadingCurrenciesMessage( locale );
+    }
+
+    //--Messages----------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausMessages
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the text of message <code>readingCurrencies</code>.
+     * <blockquote><pre>Liest Währungen.</pre></blockquote>
+     * <blockquote><pre>Reads currencies.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     *
+     * @return the text of message <code>readingCurrencies</code>.
+     */
+    private String getReadingCurrenciesMessage( final Locale locale )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "readingCurrencies", locale, null );
+
+    }
+
+// </editor-fold>//GEN-END:jdtausMessages
+
+    //----------------------------------------------------------------Messages--
+}
---
+++ jdtaus-banking-messages/src/main/java/org/jdtaus/banking/messages/ReadsTextschluesselMessage.java
@@ -0,0 +1,96 @@
+/*
+ *  jDTAUS Banking Messages
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.jdtaus.banking.messages;
+
+import java.util.Locale;
+import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.text.Message;
+
+/**
+ * Message stating that Textschlüssel are being read.
+ * @author Christian Schulte
+ * @version $Id$
+ */
+public class ReadsTextschluesselMessage extends Message
+{
+
+    /** Serial version UID for backwards compatibility with 1.11.x classes. */
+    private static final long serialVersionUID = 4267470749107250621L;
+
+    /** Empty array. */
+    private static final Object[] ARGUMENTS =
+    {
+    };
+
+    /** Creates a new {@code ReadsTextschluesselMessage} instance. */
+    public ReadsTextschluesselMessage()
+    {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return An empty array, since the message has no arguments.
+     */
+    public Object[] getFormatArguments( final Locale locale )
+    {
+        return ARGUMENTS;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return The corresponding text from the message's {@code ResourceBundle}
+     * <blockquote><pre>
+     * Reads Textschlüssel.
+     * </pre></blockquote>
+     */
+    public String getText( final Locale locale )
+    {
+        return this.getReadingTextschluesselMessage( locale );
+    }
+
+    //--Messages----------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausMessages
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the text of message <code>readingTextschluessel</code>.
+     * <blockquote><pre>Liest Textschlüssel.</pre></blockquote>
+     * <blockquote><pre>Reads Textschlüssel.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     *
+     * @return the text of message <code>readingTextschluessel</code>.
+     */
+    private String getReadingTextschluesselMessage( final Locale locale )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "readingTextschluessel", locale, null );
+
+    }
+
+// </editor-fold>//GEN-END:jdtausMessages
+
+    //----------------------------------------------------------------Messages--
+}
